50266630d6401369bd54d5cf50f6e01aede1477b
gness1804 <graham.nessler@gmail.com>
2018-01-13 15:54:10 -0600

    Create match function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -21,4 +22,11 @@ fn main() {
 
     println!("You guessed: {}", guess);
     // JS equivalent: `console.log(`You guessed: ${guess}`);`
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!");
+        Ordering::Greater => println!("Too big!");
+        Ordering::Equal => println!("You win!");
+    }
+    // sort of like a JS switch statement, I think
 }
