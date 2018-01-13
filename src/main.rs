fc486b79f2167a0c342d48a64bdaabea98cc569a
gness1804 <graham.nessler@gmail.com>
2018-01-13 14:06:09 -0600

    Finished first part of game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+    // equivalent of `let guess = ''` in JS
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+        // like `.catch(() -> {console.log('Failed to read line')})` in JS
+
+    println!("You guessed: {}", guess);
+    // JS equivalent: `console.log(`You guessed: ${guess}`);`
 }
