ab28bb8132dd846494af8d78930388bab88fba25
gness1804 <graham.nessler@gmail.com>
2018-01-14 14:31:04 -0600

    Full program now works

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,31 @@ use rand::Rng;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let secret_number: u32 = rand::thread_rng().gen_range(1, 101);
+    //added the `: u32` that isn't in the tutorial in order to practice annotating type
 
     println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
 
-    let mut guess = String::new();
+    let mut guess: String = String::new();
     // equivalent of `let guess = ''` in JS
+    // as with above example, I added the type annotation that isn't in the tutorial in order to practice it
 
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
         // like `.catch(() -> {console.log('Failed to read line')})` in JS
 
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
+
     println!("You guessed: {}", guess);
     // JS equivalent: `console.log(`You guessed: ${guess}`);`
 
     match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!");
-        Ordering::Greater => println!("Too big!");
-        Ordering::Equal => println!("You win!");
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
     }
     // sort of like a JS switch statement, I think
 }
